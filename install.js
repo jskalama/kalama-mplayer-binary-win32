802ba2fe7f5f4717d9e66f25f87be5ff5910ff58
no <no>
2018-05-18 08:52:23 +0300

    cool

--- install.js
+++ install.js
@@ -1,10 +1,16 @@
 const os = require('os');
+const fs = require('fs');
 const path = require('path');
 const unzip = require('unzip');
 const download = require('download');
 
+const basename = path.basename;
+const dirname = path.dirname;
+
+// //////
+
 const RELEASE =
-    'https://no-sourceforge.net/projects/mplayer-windows-builds/files/mplayer-svn-38055.zip/download';
+    'https://sourceforge.net/projects/mplayer-windows-builds/files/mplayer-svn-38055.zip/download';
 const RELEASE_MD5 = 'df503a4113472757e13e42bfa8464bf0';
 const BIN_PATH = path.join(__dirname, 'bin');
 
@@ -12,36 +18,56 @@ const E_DOWNLOAD = 'E_DOWNLOAD';
 const E_CHECKSUM = 'E_CHECKSUM';
 const E_UNZIP = 'E_UNZIP';
 
-const fetchArchive = () => {
-    const tmpPath = path.join(os.tmpdir(), 'mplayer.zip');
-    return failOnError(download(RELEASE, tmpPath), E_DOWNLOAD)
-        .then(
-            failOnError(() => verifyChecksum(tmpPath, RELEASE_MD5), E_CHECKSUM)
-        )
-        .then(failOnError(unpackTo(tmpPath, BIN_PATH), E_UNZIP))
-        .catch(reportError);
+const exit = (e, type) => {
+  switch (type) {
+    case E_DOWNLOAD:
+      console.error('Error downloading archive');
+      break;
+    case E_CHECKSUM:
+      console.error('Invalid checksum');
+      break;
+    case E_UNZIP:
+      console.error('Error unzipping');
+      break;
+    default:
+      console.error('Error');
+      break;
+  }
+  console.error(e.stack);
+  process.exit(1);
 };
 
-const failOnError = (prms, err) =>
-    Promise.resolve(prms).catch(e => Promise.reject(err));
-
-const reportError = e => {
-    switch (e) {
-        case E_DOWNLOAD:
-            console.error('Error downloading archive');
-            return;
-        case E_CHECKSUM:
-            console.error('Invalid checksum');
-            return;
-        case E_UNZIP:
-            console.error('Error unzipping');
-            return;
-        default:
-            console.error(e.stack);
-            return;
-    }
+const report = (message) => {
+  console.log(message);
+  return Promise.resolve();
 };
+
 const verifyChecksum = () => {};
-const unpackTo = () => {};
+
+const unpackTo = (archiveFile, destFolder) => {
+  const stream = fs
+    .createReadStream(archiveFile)
+    .pipe(unzip.Extract({ path: destFolder }));
+  return new Promise((resolve, reject) => {
+    stream.on('end', () => resolve());
+    stream.on('error', () => reject());
+  });
+};
+
+const fetchArchive = () => {
+  const tmpPath = path.join(os.tmpdir(), 'mplayer.zip');
+  return report('Downloading archive')
+    .then(() =>
+      download(RELEASE, dirname(tmpPath), {
+        filename: basename(tmpPath),
+      }))
+    .catch(e => exit(e, E_DOWNLOAD))
+    .then(() => report('Verifying checksum'))
+    .then(() => verifyChecksum(tmpPath, RELEASE_MD5))
+    .catch(e => exit(e, E_CHECKSUM))
+    .then(() => report('Unzipping'))
+    .then(() => unpackTo(tmpPath, BIN_PATH))
+    .catch(e => exit(e, E_UNZIP));
+};
 
 fetchArchive();
